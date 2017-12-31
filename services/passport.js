eecd9be44351cb41bdbc1af8ede8c84eff701d78
shiprajoshi <joshishipra0@gmail.com>
2017-12-31 11:23:13 +0530

    added prod & dev env

--- services/passport.js
+++ services/passport.js
@@ -23,10 +23,10 @@ passport.deserializeUser((id, done) => {
 passport.use(
 	new GoogleStrategy(
 	{
-	clientID: keys.googleClientID,
-	clientSecret: keys.googleClientSecret,
-	callbackURL: '/auth/google/callback',
-	proxy: true
+		clientID: keys.googleClientID,
+		clientSecret: keys.googleClientSecret,
+		callbackURL: '/auth/google/callback',
+		proxy: true
 	}, 
 	(accessToken, refreshToken, profile, done) => {
 		User.findOne({googleId: profile.id})
